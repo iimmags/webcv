1560190d629b86109477cee422df4438b2913de6
iimmags <inn.mendes.11@gmail.com>
2020-07-08 11:58:58 +0100

    Updates

---
+++ precache-manifest.ab47470a341a45e00424c5c0c53df01b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9901b828628fc77a8378e0204572c69",
+    "url": "/webcv/index.html"
+  },
+  {
+    "revision": "234ebdbfb848e85de0ae",
+    "url": "/webcv/static/css/main.172b987b.chunk.css"
+  },
+  {
+    "revision": "90f23035ed5341f83e58",
+    "url": "/webcv/static/js/2.0a4c5cab.chunk.js"
+  },
+  {
+    "revision": "234ebdbfb848e85de0ae",
+    "url": "/webcv/static/js/main.298ab12e.chunk.js"
+  },
+  {
+    "revision": "08687dcc2e9176bf704b",
+    "url": "/webcv/static/js/runtime-main.c64c6fc9.js"
+  }
+]);
