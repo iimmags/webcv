6e3371df4cc04a4fb8b2c203c0dd5a9634f4b8de
iimmags <inn.mendes.11@gmail.com>
2021-04-12 15:43:29 +0100

    feat: update resume info

--- src/components/cardGroup/CardsGroup.tsx
+++ src/components/cardGroup/CardsGroup.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import {
   Card,
   CardActionArea,
@@ -8,81 +7,99 @@ import {
   CardHeader,
   Avatar,
 } from "@material-ui/core";
-
-import { cardsList } from "../../data/cardsList";
+import { cardsSections } from "../../data/cards.json";
+import { v4 as uuidv4 } from "uuid";
 
 const CardsGroup: React.FC = () => {
   const [cardsInfo, setCardsInfo] = useState<any>();
 
   useEffect(() => {
-    setCardsInfo(cardsList);
+    setCardsInfo(cardsSections);
   }, []);
 
-  const renderAvatarElement = (avatarSrc: string) => {
-    if (avatarSrc !== null) {
-      const src =  import(avatarSrc);
+  const renderAvatarElement = (avatarSrc: string) =>
+    avatarSrc !== null ? <Avatar src={avatarSrc} /> : <></>;
 
-      return <Avatar src={src} />;
-    }
+  const renderJobDescription = (jobDescription: string) => (
+    <p className="mb-3" key={uuidv4()}>
+      {jobDescription}
+    </p>
+  );
 
-    return <></>;
-  };
+  const renderLocation = ({ locationDate, locationMonths }: any) => (
+    <>
+      <span>{locationDate}</span>
+      {locationMonths && (
+        <>
+          <span className="ml-2 mr-2">•</span>
+          <span>{locationMonths}</span>
+        </>
+      )}
+    </>
+  );
 
-  return cardsInfo
-    ? cardsInfo.map((cardInfo: any, cardInfoIndex: number) => {
-        return (
-          <>
-            <div className="mb-20"></div>
+  const renderCardInfo = (
+    {
+      avatarSrc,
+      location,
+      locationDate,
+      locationMonths,
+      jobExperienceName,
+      jobExperienceRole,
+      jobExperienceDescriptions,
+    }: any,
+    isCertifications: boolean
+  ) => (
+    <div key={uuidv4()}>
+      <Card>
+        <CardActionArea className="hover:bg-gray-200 focus:outline-none">
+          <CardHeader
+            avatar={renderAvatarElement(avatarSrc)}
+            title={location}
+            subheader={renderLocation({ locationDate, locationMonths })}
+          />
+          <CardContent>
             <Typography
               gutterBottom
-              variant="h6"
-              component="h2"
-              className="bg-gray-200 rounded pl-1"
-              id={`${cardInfo.typoName}-${cardInfoIndex}`}
+              variant={isCertifications ? "subtitle1" : "h5"}
             >
-              {cardInfo.typoName}
+              {jobExperienceName}
             </Typography>
-            <div className="mb-5"></div>
-            {cardInfo.cards &&
-              cardInfo.cards.map((card: any, cardIndex: number) => {
-                return (
-                  <>
-                    <Card id={`${cardInfo.typoName}-card-${cardIndex}`}>
-                      <CardActionArea className="hover:bg-gray-200 focus:outline-none">
-                        <CardHeader
-                          avatar={renderAvatarElement(card.avatarSrc)}
-                          title={card.cardHeaderTitle}
-                          subheader={card.cardHeaderSubHeader}
-                        />
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {card.jobExperienceName}
-                          </Typography>
-                          <Typography
-                            gutterBottom
-                            variant="subtitle1"
-                            component="h2"
-                          >
-                            {card.jobExperienceRole}
-                          </Typography>
-                          <Typography
-                            variant="body2"
-                            color="textSecondary"
-                            component="p"
-                          >
-                            {card.jobExperienceDescription}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                    <div className="mb-6"></div>
-                  </>
-                );
-              })}
-          </>
-        );
-      })
-    : null;
+            <Typography
+              gutterBottom
+              variant={isCertifications ? "subtitle2" : "subtitle1"}
+            >
+              {jobExperienceRole}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="div">
+              {jobExperienceDescriptions?.map(renderJobDescription)}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <div className="mb-6"></div>
+    </div>
+  );
+
+  const renderCardsSections = ({ typoName, cards, isCertifications }: any) => {
+    return (
+      <div key={uuidv4()}>
+        <div className="mb-20"></div>
+        <Typography
+          gutterBottom
+          variant="h6"
+          component="h2"
+          className="bg-gray-200 rounded pl-1"
+        >
+          {typoName}
+        </Typography>
+        <div className="mb-5"></div>
+        {cards?.map((card: any) => renderCardInfo(card, isCertifications))}
+      </div>
+    );
+  };
+
+  return cardsInfo ? cardsInfo.map(renderCardsSections) : <></>;
 };
 
 export default CardsGroup;
--- src/components/navbar/AppNavBar.tsx
+++ src/components/navbar/AppNavBar.tsx
@@ -16,25 +16,26 @@ const AppNavBar: React.FC = () => {
       <AppBar position="static">
         <div className="p-0 pt-10 bg-gray-200">
           <Toolbar>
-          <div className="mr-6">
-            <Avatar
-              src={require('../../assets/avatar.jpg')}
-            ></Avatar>
+            <div className="mr-6">
+              <Avatar src={require("../../assets/avatar.jpg")}></Avatar>
             </div>
             <div className="flex-grow text-gray-900">
-            <Typography variant="h4">
-              Inês Portfolio
-            </Typography>
+              <Typography variant="h4">Inês Magalhães</Typography>
             </div>
-            <IconButton className="focus:outline-none" edge="start" aria-label="menu" >
+            {/* <IconButton
+              className="focus:outline-none"
+              edge="start"
+              aria-label="menu"
+            >
               <MenuIcon fontSize="large" />
-            </IconButton>
+            </IconButton> */}
           </Toolbar>
         </div>
-        <p className="bg-gray-200 pb-10 pl-5 text-gray-900">
-          Inês Mendes Magalhães, I was born in Porto and I'm 23 years old.{" "}
-          <br />I currently live in Coimbra, and I am currently working as a
-          Front-End Developer at Talkdesk.
+        <p className="bg-gray-200 pb-10 pl-5 text-gray-700">
+          <br />• 4+ of experience in engineering solutions with a wide range of
+          e-commerce application and technology skills.
+          <br />• Leverage front-end expertise to build interactive and
+          user-centered websites.
         </p>
       </AppBar>
     </div>
